630270f67885c5f2a340a151e5f5b6c520930d00
Max Carvajal <jcarvajal288@gmail.com>
2024-07-21 18:27:46 -0500

    Move hexmap draw code into hex_map.rs

--- src/hex_map.rs
+++ src/hex_map.rs
@@ -1,9 +1,23 @@
+use bevy::asset::{AssetServer, Handle};
+use bevy::prelude::{Commands, default, Image, Res, SpriteBundle, Transform, Vec2};
 use bevy::utils::HashMap;
 
 use crate::cubic::{create_axial, Cubic};
 
+const HEX_SIZE: f32 = 64.;
+
 pub struct HexMap {
     pub map: HashMap<Cubic, bool>
+}
+
+impl HexMap {
+    pub fn draw(&self, mut commands: Commands, asset_server: Res<AssetServer>) {
+        let clear001: Handle<Image> = asset_server.load("Clear/Clear001.png");
+        for hex in &self.map {
+            let cubic = hex.0;
+            draw_hex(&mut commands, clear001.clone(), create_axial([cubic.q(), cubic.r()]));
+        }
+    }
 }
 
 pub fn create_hex_map(left: i16, right: i16, top: i16, bottom: i16) -> HexMap {
@@ -15,4 +29,17 @@ pub fn create_hex_map(left: i16, right: i16, top: i16, bottom: i16) -> HexMap {
         }
     }
     return HexMap { map: new_map }
+}
+
+fn draw_hex(commands: &mut Commands, hex_texture: Handle<Image>, cubic: Cubic) {
+    let pixel_position: Vec2 = cubic.to_pixel(HEX_SIZE);
+    commands.spawn(SpriteBundle {
+        texture: hex_texture,
+        transform: Transform {
+            translation: (pixel_position, 0.).into(),
+            scale: (0.25, 0.25, 0.25).into(),
+            ..default()
+        },
+        ..default()
+    });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use bevy::prelude::*;
+
+use crate::hex_map::{create_hex_map, HexMap};
+
 mod cubic;
 mod hex_map;
-
-use bevy::prelude::*;
-use crate::cubic::{create_axial, create_cubic, Cubic};
-use crate::hex_map::{create_hex_map, HexMap};
 
 fn main() {
     App::new()
@@ -12,33 +12,11 @@ fn main() {
         .run();
 }
 
-const HEX_SIZE: f32 = 64.;
-
 fn setup(mut commands: Commands, mut clear_color: ResMut<ClearColor>, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
     clear_color.0 = Color::hsl(210., 1., 0.8);
-    draw_map(commands, asset_server);
-}
-
-fn draw_map(mut commands: Commands, asset_server: Res<AssetServer>) {
     let hex_map: HexMap = create_hex_map(-3, 3, -2, 2);
-    let clear001: Handle<Image> = asset_server.load("Clear/Clear001.png");
-    for hex in hex_map.map {
-        let cubic = hex.0;
-        draw_hex(&mut commands, clear001.clone(), create_axial([cubic.q(), cubic.r()]));
-    }
+    hex_map.draw(commands, asset_server);
 }
 
-fn draw_hex(commands: &mut Commands, hex_texture: Handle<Image>, cubic: Cubic) {
-    let pixel_position: Vec2 = cubic.to_pixel(HEX_SIZE);
-    commands.spawn(SpriteBundle {
-        texture: hex_texture,
-        transform: Transform {
-            translation: (pixel_position, 0.).into(),
-            scale: (0.25, 0.25, 0.25).into(),
-            ..default()
-        },
-        ..default()
-    });
-}
 
